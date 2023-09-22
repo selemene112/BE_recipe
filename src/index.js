bf823bbd59a4e4e6d1b90ddacf8467d819d7e2e5
selemene112 <ariyanda98y@gmail.com>
2023-09-22 10:12:00 +0800

    add web Socket for getting data real time

--- src/index.js
+++ src/index.js
@@ -4,15 +4,39 @@ const cors = require('cors');
 const PORT = process.env.PORT;
 const bodyParser = require('body-parser');
 
+//========================================== Call ROute ================================================
+
 const hutan = require('./route/UserRoute');
 const recipe = require('./route/Reciperoute');
 const Auth = require('./route/auth');
 const comment = require('./route/comment');
 const like = require('./route/likeroute');
+const { CreateComment } = require('./model/coment');
+
+//========================================== END Call ROute ================================================
 
 app.use(cors());
 
 app.use(express.json());
+
+//========================================== Web Socket Commentar ================================================
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected.');
+
+  socket.on('chat message', async (message) => {
+    const comment = await CreateComment(message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected.');
+  });
+});
+
+//========================================== End Web Socket Commentar ================================================
 
 app.get('/api/v1/', (req, res) => {
   res.status(200).json({
